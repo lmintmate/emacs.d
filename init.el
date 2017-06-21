1983bd2d1ead6ef8545201e5cb708fa79263a56d
lmintmate <marynik37@hotmail.gr>
2017-06-21 14:00:08 +0300

    changed fringe color of blue-mood

--- init.el
+++ init.el
@@ -43,7 +43,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(fringe ((t (:background "DodgerBlue4")))))
 
 ;; greek calendar
 (setq calendar-week-start-day 1
