321abf360ea66c8011b153a88178d63f9a899019
lmintmate <marynik37@hotmail.gr>
2017-06-21 21:36:29 +0300

    disabled autosaves

--- init(blue-mood-version).el
+++ init(blue-mood-version).el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-save-default nil)
  '(delete-selection-mode t)
  '(geiser-active-implementations (quote (guile racket chez mit chibi)))
  '(geiser-racket-binary "~/racket/bin/racket")
