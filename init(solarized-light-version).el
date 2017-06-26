39aa1deee9fa57cf98f4280003956e76c144cab5
lmintmate <marynik37@hotmail.gr>
2017-06-26 10:54:44 +0300

    buffer defuns in separate file

--- init(solarized-light-version).el
+++ init(solarized-light-version).el
@@ -34,6 +34,9 @@
 ;; nationality parameters - not included in the version control, for obvious reasons. The list of relevant parameters is however included in nationality-parameters.txt.
 (load-file "~/.emacs.d/nationality-parameters.el")
 
+;; settings for multiple buffer management
+(load-file "~/.emacs.d/buffer-defuns.el")
+
 ;; other config parameters
 (setq auto-save-default nil)
 (setq cfw:display-calendar-holidays nil)
@@ -129,32 +132,3 @@
   (defun turn-off-menu-with-olivetti ()
     (menu-bar-mode -1))
   (add-hook 'olivetti-mode-hook 'turn-off-menu-with-olivetti))
-
-;; change layout of windows from horizontal to vertical very easily
-;; (from http://whattheemacsd.com/buffer-defuns.el-03.html)
-(defun toggle-window-split ()
-  (interactive)
-  (if (= (count-windows) 2)
-      (let* ((this-win-buffer (window-buffer))
-             (next-win-buffer (window-buffer (next-window)))
-             (this-win-edges (window-edges (selected-window)))
-             (next-win-edges (window-edges (next-window)))
-             (this-win-2nd (not (and (<= (car this-win-edges)
-                                         (car next-win-edges))
-                                     (<= (cadr this-win-edges)
-                                         (cadr next-win-edges)))))
-             (splitter
-              (if (= (car this-win-edges)
-                     (car (window-edges (next-window))))
-                  'split-window-horizontally
-                'split-window-vertically)))
-        (delete-other-windows)
-        (let ((first-win (selected-window)))
-          (funcall splitter)
-          (if this-win-2nd (other-window 1))
-          (set-window-buffer (selected-window) this-win-buffer)
-          (set-window-buffer (next-window) next-win-buffer)
-          (select-window first-win)
-          (if this-win-2nd (other-window 1))))))
-;; the keyboard shortcut for the above function
-(define-key global-map "\M-]" 'toggle-window-split)
