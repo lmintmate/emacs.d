e5d096eb12f5de0394d4ff628397322f003df43a
lmintmate <marynik37@hotmail.gr>
2017-06-26 11:00:18 +0300

    new multiple buffer function

--- buffer-defuns.el
+++ buffer-defuns.el
@@ -26,3 +26,32 @@
           (if this-win-2nd (other-window 1))))))
 ;; the keyboard shortcut for the above function
 (define-key global-map "\M-]" 'toggle-window-split)
+
+;; flips 2 window frame, so that left goes right, and up goes down
+;; (from http://whattheemacsd.com/buffer-defuns.el-02.html)
+(defun rotate-windows ()
+  "Rotate your windows"
+  (interactive)
+  (cond ((not (> (count-windows)1))
+         (message "You can't rotate a single window!"))
+        (t
+         (setq i 1)
+         (setq numWindows (count-windows))
+         (while  (< i numWindows)
+           (let* (
+                  (w1 (elt (window-list) i))
+                  (w2 (elt (window-list) (+ (% i numWindows) 1)))
+
+                  (b1 (window-buffer w1))
+                  (b2 (window-buffer w2))
+
+                  (s1 (window-start w1))
+                  (s2 (window-start w2))
+                  )
+             (set-window-buffer w1  b2)
+             (set-window-buffer w2 b1)
+             (set-window-start w1 s2)
+             (set-window-start w2 s1)
+             (setq i (1+ i)))))))
+;; the keyboard shortcut for the above function
+(define-key global-map "\M-[" 'rotate-windows)
