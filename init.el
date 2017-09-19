5a2c2fd33e1d06769135951011c1e1f0e0592645
lmintmate <marynik37@hotmail.gr>
2017-09-19 07:46:56 +0300

    Document init.el in the literate config as well

--- init.el
+++ init.el
@@ -1,11 +1,9 @@
-;; I keep the custom-set-variables here in case I wish to change something in the Customize interface in the future
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
 )
-;; adds melpa repository
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
