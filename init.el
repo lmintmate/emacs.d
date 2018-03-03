a2eba5f51ad51e13cd6b581751ed09340b883ff0
lmintmate <marynik37@hotmail.gr>
2018-03-03 15:11:51 +0200

    Remove custom-set-variables
    Since I now have the settings from Customize be written in a separate
    file, this needen't be present here.

--- init.el
+++ init.el
@@ -1,9 +1,3 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
-)
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
