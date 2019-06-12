4a36dcab2f2a62848853c682dd504fefeaf06612
lmintmate <marynik37@hotmail.gr>
2019-06-12 15:57:17 +0300

    :wrench: set-face-attribute mode-line-buffer-id

--- init.el
+++ init.el
@@ -105,9 +105,7 @@ vimrc-mode)
 (set-face-attribute 'mode-line nil :background "grey75" :foreground "black" :box '(:line-width 2 :color "grey75"))
 (set-face-attribute 'mode-line-inactive nil :background "grey30" :foreground "grey80" :box '(:line-width 2 :color "grey30"))
 (set-face-attribute 'mode-line-highlight nil :box '(:line-width 1 :color "grey20"))
-
-(custom-set-faces
- '(mode-line-buffer-id ((t (:weight normal)))))
+(set-face-attribute 'mode-line-buffer-id nil :weight 'normal)
 
 (set-face-attribute 'vertical-border nil :foreground (face-attribute 'fringe :background))
 
