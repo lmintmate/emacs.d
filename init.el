fed29085b588594714ab3e9e9780e04ae5da556f
lmintmate <marynik37@hotmail.gr>
2019-07-31 10:54:02 +0300

    :wrench: Set sw to go to start of match, separate binding for sw-isearch

--- init.el
+++ init.el
@@ -826,7 +826,8 @@ initialized with the current directory instead of filename."
 
 (setq ivy-count-format "(%d/%d) ")
 
-(global-set-key (kbd "C-s") 'swiper-isearch)
+(global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "C-M-s") 'swiper-isearch)
 (global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
 (global-set-key (kbd "\C-xb") 'counsel-switch-buffer)
@@ -840,6 +841,8 @@ initialized with the current directory instead of filename."
 (setq ivy-initial-inputs-alist nil)
 
 (setq ivy-extra-directories nil)
+
+(setq swiper-goto-start-of-match t)
 
 (setcdr (assq t ivy-format-functions-alist) #'ivy-format-function-line)
 
