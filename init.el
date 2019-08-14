b273af1bb017851250933f18ea12b810e1ce57c8
lmintmate <marynik37@hotmail.gr>
2019-08-14 16:17:51 +0300

    :wrench: Add org-split-block function
    For some reason its docstring confused the org babel tangling, so I had
    to make it a separate example block.

--- init.el
+++ init.el
@@ -884,6 +884,65 @@ initialized with the current directory instead of filename."
 
 (push '(org-goto . tree) org-show-context-detail)
 
+(defun org-in-any-block-p ()
+  "Return non-nil if the point is in any Org block.
+The Org block can be *any*: src, example, verse, etc., even any
+Org Special block.
+This function is heavily adapted from `org-between-regexps-p'."
+  (save-match-data
+    (let ((pos (point))
+	  (case-fold-search t)
+	  (block-begin-re "^[[:blank:]]*#\\+begin_\\(?1:.+?\\)\\(?: .*\\)*$")
+	  (limit-up (save-excursion (outline-previous-heading)))
+	  (limit-down (save-excursion (outline-next-heading)))
+	  beg end)
+      (save-excursion
+	;; Point is on a block when on BLOCK-BEGIN-RE or if
+	;; BLOCK-BEGIN-RE can be found before it...
+	(and (or (org-in-regexp block-begin-re)
+		 (re-search-backward block-begin-re limit-up :noerror))
+	     (setq beg (match-beginning 0))
+	     ;; ... and BLOCK-END-RE after it...
+	     (let ((block-end-re (concat "^[[:blank:]]*#\\+end_"
+					 (match-string-no-properties 1)
+					 "\\( .*\\)*$")))
+	       (goto-char (match-end 0))
+	       (re-search-forward block-end-re limit-down :noerror))
+	     (> (setq end (match-end 0)) pos)
+	     ;; ... without another BLOCK-BEGIN-RE in-between.
+	     (goto-char (match-beginning 0))
+	     (not (re-search-backward block-begin-re (1+ beg) :noerror))
+	     ;; Return value.
+	     (cons beg end))))))
+
+(defun org-split-block ()
+  (interactive)
+  (if (org-in-any-block-p)
+      (save-match-data
+	(save-restriction
+	  (widen)
+	  (let ((case-fold-search t)
+		(at-bol (bolp))
+		block-start
+		block-end)
+	    (save-excursion
+	      (re-search-backward "^\\(?1:[[:blank:]]*#\\+begin_.+?\\)\\(?: .*\\)*$" nil nil 1)
+	      (setq block-start (match-string-no-properties 0))
+	      (setq block-end (replace-regexp-in-string
+			       "begin_" "end_" ;Replaces "begin_" with "end_", "BEGIN_" with "END_"
+			       (match-string-no-properties 1))))
+	    ;; Go to the end of current line, if not at the BOL
+	    (unless at-bol
+	      (end-of-line 1))
+	    (insert (concat (if at-bol "" "\n")
+			    block-end
+			    "\n\n"
+			    block-start
+			    (if at-bol "\n" "")))
+	    ;; Go to the line before the inserted "#+begin_ .." line
+	    (beginning-of-line (if at-bol -1 0)))))
+    (message "Point is not in an Org block")))
+
 (require 'undo-tree)
 
 (global-undo-tree-mode)
